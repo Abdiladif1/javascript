3ea378bfb21fb87de45be8b1cbdd6d75f9b2d39c
Abdiladif1 <youngkhalifa1479@gmail.com>
2025-07-27 14:33:32 +0300

    I added Objects

---
+++ objects/objects.js
@@ -0,0 +1,51 @@
+
+// let student1 = {
+//  firstName : "Farhiya",
+//  lastName : "Ali",
+//  age : 30
+// }
+
+
+// let student2 = {
+//  firstName : "Mohamed",
+//  lastName : "Bilal",
+//  age : 20,
+// }
+
+// console.log(student1.firstName)
+
+
+// let person = new Object()
+// person.firstName = "Ali"
+// person.age = 20
+
+// person.walk = function () {
+//   console.log("this person is walking")
+// }
+
+// person.walk()
+
+// let person = {}
+// console.log(person)
+// person["firstName"] = "Ahmed"
+
+// console.log(person["firstName"])
+
+// let {firstName} = person
+
+// console.log(firstName)
+
+// let person = {
+//  firstName : "Maryan",
+//  lastName : "Burhan",
+//  age : 20,
+//  isMarried : false 
+// }
+
+// for (let prop in person){
+//   console.log(person[prop])
+// }
+
+
+
+
