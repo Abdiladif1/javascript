ba5074a406254a2e208217e1509bf964d8536f46
Abdiladif1 <youngkhalifa1479@gmail.com>
2025-07-31 23:59:44 +0300

    adding arrow functions

---
+++ arrow-functions/arrow.js
@@ -0,0 +1,15 @@
+// function sayMyName () {
+//   console.log("Hello" + "Ahmed")
+// }
+
+
+// let sayMyName =  () => {
+//   console.log("Hello " + "Ahmed")
+// }
+
+
+// let sayMyName =  name =>  "Hello " + name
+
+
+// let name = sayMyName("Ahmed") 
+// console.log(name)
